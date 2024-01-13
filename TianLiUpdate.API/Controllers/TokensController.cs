54098ecd9cf4853593c1ddb86e507ab7ae4d1fd1
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2024-01-14 01:06:21 +0800

    update:添加depends接口。调整token接口

--- TianLiUpdate.API/Controllers/TokensController.cs
+++ TianLiUpdate.API/Controllers/TokensController.cs
@@ -15,65 +15,20 @@ namespace TianLiUpdate.API.Controllers
             _context = context;
         }
         
-        /*
-        // GET: /Tokens
-        [HttpGet]
-        public ActionResult<IEnumerable<Token>> GetTokens()
-        {
-            if (_context.Tokens == null)
-            {
-                return NotFound("Entity set 'Tokens'  is null.");
-            }
-            return _context.Tokens.ToList();
-        }
-        // GET: api/Tokens/5
-        [HttpGet("{id}")]
-        public ActionResult<Token> GetToken(Guid? id)
-        {
-            if (_context.Tokens == null)
-            {
-                return NotFound("Entity set 'Tokens'  is null.");
-            }
-            var token = _context.Tokens.Find(id);
-            if (token == null)
-            {
-                return NotFound("Token not found.");
-            }
-            return token;
-        }
-        // PUT: api/Tokens/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public IActionResult PutToken(Guid? id, Token token)
-        {
-            if (id != token.Id)
-            {
-                return BadRequest("Token id is not equal to the id in the request body.");
-            }
-            _context.Entry(token).State = EntityState.Modified;
-            try
-            {
-                _context.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!TokenExists(id))
-                {
-                    return NotFound("Token not found.");
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return NoContent();
-        }
-        
         // POST: api/Tokens
         // Create a new token
-        [HttpPost("Create")]
-        public ActionResult<Token> PostToken()
+        [HttpGet("Create")]
+        public ActionResult<Token> PostToken(string suToken)
         {
+            var su = GetSuperToken();
+            if(su==null)
+            {
+                return NotFound("Super token not found");
+            }
+            if (suToken != su.TokenString)
+            {
+                return Unauthorized();
+            }
             var token = new Token{
                 TokenID = Guid.NewGuid(),
                 TokenString = Guid.NewGuid().ToString(),
@@ -83,50 +38,10 @@ namespace TianLiUpdate.API.Controllers
             _context.SaveChanges();
             return Ok(token.TokenString);
         }
-        */
-        //[HttpPost("CreateTokenString")]
-        //public ActionResult<Token> PostTokenString(string tokenString, string suToken)
-        //{
-        //    if (_context.Tokens.Where(t => t.TokenString == suToken & t.TokenID == Guid.Empty).Count()== 0)
-        //    {
-        //        return Problem("Super user token is not valid.");
-        //    }
-        //    if (_context.Tokens == null)
-        //    {
-        //        return Problem("Entity set 'ArtifactContext.Tokens'  is null.");
-        //    }
-        //    var token = new Token
-        //    {
-        //        TokenID = Guid.NewGuid(),
-        //        TokenString = tokenString,
-        //        LastUseTime = DateTime.Now
-        //    };
-        //    _context.Tokens.Add(token);
-        //    _context.SaveChanges();
-        //    return Ok(token); //CreatedAtAction("GetToken", new { id = token.Id }, token);
-        //}
-        /*
-        // DELETE: api/Tokens/5
-        [HttpDelete("{id}")]
-        public IActionResult DeleteToken(Guid? id)
+
+        private Token? GetSuperToken()
         {
-            if (_context.Tokens == null)
-            {
-                return NotFound("Entity set 'Tokens'  is null.");
-            }
-            var token = _context.Tokens.Find(id);
-            if (token == null)
-            {
-                return NotFound("Token not found.");
-            }
-            _context.Tokens.Remove(token);
-            _context.SaveChanges();
-            return NoContent();
-        }
-        */
-        private bool TokenExists(Guid? id)
-        {
-            return (_context.Tokens?.Any(e => e.TokenID == id)).GetValueOrDefault();
+            return _context.Tokens.Find(Guid.Empty.ToString());
         }
     }
 }
--- TianLiUpdate.API/Models/FileHash.cs
+++ TianLiUpdate.API/Models/FileHash.cs
@@ -5,7 +5,8 @@ namespace TianLiUpdate.API.Models
         public Guid FileHashID { get; set; } = Guid.Empty;
         public string Hash { get; set; } = string.Empty;
         public string FileName { get; set; } = string.Empty;
+        public string FilePath { get; set; } = string.Empty;
         public string DownloadUrl { get; set; } = string.Empty;
-        //public ProjectItem Project { get; set; } = new ProjectItem();
+        //public Project Project { get; set; } = new Project();
     }
 }
